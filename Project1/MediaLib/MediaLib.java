2a718ecca232cd9018c6c0388a90b7847850b329
Simmonds7 <kristofer.simmonds@gmail.com>
2019-02-08 11:17:17 -0600

    Project 1 start

---
+++ Project1/MediaLib/MediaLib.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class MediaLib here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MediaLib
+{
+    static void main()
+    {
+        System.out.println("Welcome to your Media Library!");
+        Song song1 = new Song();
+        System.out.println(song1);
+    }
+}
---
+++ Project1/MediaLib/Song.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Song here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Song
+{
+    // instance variables - replace the example below with your own
+    private int rating;
+    private String title;
+    
+    public Song()
+    {
+        // initialise instance variables
+        rating = 0;
+        title = "";
+    }
+}
